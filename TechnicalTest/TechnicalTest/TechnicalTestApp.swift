bc1991277d9d538df40ac0f489a4b60aad2308eb
Ainhoa <ainhoacro@ext.zara.com>
2025-05-29 12:09:36 +0200

    initial commit

---
+++ TechnicalTest/TechnicalTest/TechnicalTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechnicalTestApp.swift
+//  TechnicalTest
+//
+//  Created by Ainhoa Calviño Rodríguez on 29/5/25.
+//
+
+import SwiftUI
+
+@main
+struct TechnicalTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TechnicalTest/TechnicalTestTests/TechnicalTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  TechnicalTestTests.swift
+//  TechnicalTestTests
+//
+//  Created by Ainhoa Calviño Rodríguez on 29/5/25.
+//
+
+import Testing
+@testable import TechnicalTest
+
+struct TechnicalTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
